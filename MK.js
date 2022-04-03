a7c58dba8a97b9afa4d19d307d8aec6a200f7962
Coral Kuta <coral,kuta@live,biu.ac.il>
2022-04-03 22:28:12 +0300

    launch screen design

---
+++ MK.js
@@ -0,0 +1,19 @@
+
+
+// function MK() {
+//     return (
+//         <div className="MK">
+//         <header className="MK-Header">
+        
+    
+//                 <a class="btn btn-primary" href="#" role="button">Link</a>
+//                 <button class="btn btn-primary" type="submit">Button</button>
+//                 <input class="btn btn-primary" type="button" value="Input"> </input>
+//                     <input class="btn btn-primary" type="submit" value="Submit"></input>
+//                         <input class="btn btn-primary" type="reset" value="Reset"></input>
+//         </header>
+//         </div>
+//     );
+// }
+
+// export default MK;
---
+++ launch.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './launch.css';
+// import MK from './MK';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <MK />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
+// reportWebVitals();
