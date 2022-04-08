59c76a2612fec609d56b23fcd1c715e7739de327
Tal Dabran <tal.dabran@live.biu.ac.il>
2022-04-08 17:49:56 +0300

    attached components icons has been added

--- src/TypingBoard/TypingBoard.js
+++ src/TypingBoard/TypingBoard.js
@@ -1,39 +1,51 @@
 import './TypingBoard.css';
+import AttachComponent from './attachComponent/AttachComponent';
 import React, { Component } from 'react';
 import { useRef } from 'react';
 import { useState } from 'react';
+
 
 //TODO - 
 //       1. validity check before sending (don't send message with blank chars)
 //       2. flexible textarea when breakline comes without damaging the icons size
 
 
-function TypingBoard({setter}) {
+function TypingBoard({ setter }) {
   const textBoard = useRef(null);
 
-  
+
 
   const send = function () {
     //send message if it isn't empty
-    if(textBoard.current.value != ""){
-      console.log(textBoard.current.value);
+    if (textBoard.current.value != "") {
       setter(textBoard.current.value);
+      document.getElementById("attached").style.display = "none"
       //clean the text board (input field)
-      textBoard.current.value='';
+      textBoard.current.value = '';
     }
+  }
+  const attach = function () {
+    if (document.getElementById("attached").style.display != "block")
+      document.getElementById("attached").style.display = "block"
+    else
+      document.getElementById("attached").style.display = "none"
   }
 
 
   return (
     <div className="gray-low-panel d-flex">
-      <svg id="attachBtn" xmlns="http://www.w3.org/2000/svg" className="bi bi-paperclip" viewBox="0 0 16 16">
+      <div id="attached" className="attached">
+        <AttachComponent />
+      </div>
+      <svg onClick={attach} xmlns="http://www.w3.org/2000/svg" className="bi bi-paperclip" viewBox="0 0 16 16">
         <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z" />
       </svg>
-      <textarea ref={textBoard}  className="message-board w-80" placeholder="New message here..."></textarea>
-      <svg onClick={send} xmlns="http://www.w3.org/2000/svg" className="bi bi-send" viewBox="0 0 16 16" >
-        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+      <textarea ref={textBoard} className="message-board w-80" placeholder="New message here..."></textarea>
+      <svg onClick={send} xmlns="http://www.w3.org/2000/svg" className="bi  bi-arrow-right-circle-fill" viewBox="0 0 16 16" >
+        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
       </svg>
     </div>
+
   );
 }
 
---
+++ src/TypingBoard/attachComponent/AttachComponent.js
@@ -0,0 +1,30 @@
+import './AttachComponent.css';
+
+
+
+
+
+function AttachComponent() {
+
+    return (
+        <div className="attach-panel">
+            <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-camera-fill" viewBox="0 0 16 16">
+                <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-camera-video-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-mic-fill" viewBox="0 0 16 16">
+                <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z" />
+                <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+            </svg>
+        </div>
+
+    );
+}
+
+export default AttachComponent;
