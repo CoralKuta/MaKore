654747b0d61cb18e6ec4630707a592281f5f7329
idota <IdoTavron7>
2022-04-09 21:28:18 +0300

    Finishing the routes for other chats

--- src/ContactItem/Contacts.js
+++ src/ContactItem/Contacts.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function ContactsComp(contact) {
     return (
         <div tabIndex={"1"} className="list-group-item list-group-item-action" >
-            <Link to={`/chats/${contact.nameCon}`}>
+            <Link to={ `/chats/${contact.nameCon}`} state = {{contact : contact}}>
                 <div className="img"> <img src={contact.image} className="rounded-circle" width="40px" alt=""></img></div>
                 <h4 className="ContractName">{contact.nameCon} </h4>
                 {contact.time !== 0 ? <div className="time">{contact.time}</div> : null}
--- src/MessageHead/MessageHead.js
+++ src/MessageHead/MessageHead.js
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const MessageHead = () => {
-    const {name} = useParams();
-
+    const { name } = useParams();
+    const { contact } = useLocation().state;
+    console.log(name);
+    console.log(contact);
     return (
-        <div className="message-head">
-        <h2>Message Head - {name}</h2>
+        <div className="MemmberInfo">
+            <div className="chat-image"> <img src={contact.image} className="rounded-circle" width="40px" alt=""></img></div>
+            <div className="chat-Name" id="MemberName">
+                <h4>{contact.nameCon}</h4>
+            </div>
+            <div className="NickName">Idodo</div>
         </div>
     );
 }
