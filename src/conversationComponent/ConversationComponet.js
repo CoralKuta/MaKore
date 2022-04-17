0ec030fb64486125b65e1109cd5038db0b8009c8
Tal Dabran <tal.dabran@live.biu.ac.il>
2022-04-17 11:58:25 +0300

    The scrollbar does not extend automatic  all the way to the edge, and the reply message is sent twice.

--- src/conversationComponent/ConversationComponet.js
+++ src/conversationComponent/ConversationComponet.js
@@ -4,7 +4,7 @@ import ConvBoard from '../convBoard/ConvBoard';
 import Message from '../message/Message'
 import React, { useState } from 'react';
 
-function ConversationComponent({friend}) {
+function ConversationComponent({ friend }) {
 
   const friendData = friend[0];
   var friendChat = friend[1];
@@ -22,23 +22,19 @@ function ConversationComponent({friend}) {
     var time = today.getHours() + ":" + today.getMinutes();
 
 
-  
-    const autoReply = function () {
-      var today = new Date();
-          if (today.getMinutes() < 10)
-            var time = today.getHours() + ":0" + today.getMinutes();
-          else
-            var time = today.getHours() + ":" + today.getMinutes();
-      setMessageList([messageList, <Message content={[4, 'nothing', time]} />]);
-    }
-    if (reply){
-      setTimeout(() => {autoReply();console.log("this is the first message")}, 2000);
-      setReply(false);
-      setAlredyReply(true);
-    }
-  
-  
-  
+
+  const autoReply = function () {
+    setMessageList(friendChat.push(<Message content={[4, 'nothing', time]} />));
+  }
+
+  if (reply) {
+    setTimeout(() => { autoReply();}, 2000);
+    setReply(false);
+    setAlredyReply(true);
+  }
+
+
+
 
 
   return (
@@ -52,9 +48,10 @@ function ConversationComponent({friend}) {
         console.log(friendChat);
         let element = document.querySelector('.chat-background');
         element.scrollTop = element.scrollHeight;
-        if(!alredyReply){
-        setReply(true);
-      }}} />
+        if (!alredyReply) {
+          setReply(true);
+        }
+      }} />
     </div>
 
   );
